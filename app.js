38ebec2b7836b98b6c0fd4aa914139e10030afcc
Murtaza M <mkm177@gmail.com>
2018-12-30 21:58:50 -0800

    first commit

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const graphQlHttp = require('express-graphql');
+const mongoose = require('mongoose');
+
+const graphQlSchema = require('./graphql/schema/index');
+const graphQlResolvers = require('./graphql/resolvers/index');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+
+// app.get('/', (req,res,next) => {
+//     res.send("Hello World!");
+// })
+
+app.use('/graphql', graphQlHttp({
+    schema: graphQlSchema,
+    rootValue: graphQlResolvers,
+    graphiql: true
+
+})
+);
+
+mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@graphqlcluster-rnxvn.mongodb.net/${process.env.MONGO_DB}?retryWrites=true`).
+    then(() => {
+        app.listen(3000);
+    }).catch(err => 
+        {console.log(err);
+    })
